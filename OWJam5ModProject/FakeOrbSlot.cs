eb934dee20aac637b2ec035faade8cfd05b108a6
JohnCorby <williamforgames6@gmail.com>
2025-07-07 18:46:23 -0700

    fake orb slot that actually works

--- OWJam5ModProject/FakeOrbSlot.cs
+++ OWJam5ModProject/FakeOrbSlot.cs
@@ -1,4 +1,8 @@
-﻿using NewHorizons;
+﻿#define DEBUG_VELOCITY
+
+using System;
+using HarmonyLib;
+using NewHorizons;
 using UnityEngine;
 
 namespace OWJam5ModProject;
@@ -8,32 +12,50 @@ namespace OWJam5ModProject;
 /// </summary>
 [UsedInUnityProject]
 [RequireComponent(typeof(NomaiInterfaceOrb))]
+[HarmonyPatch]
 public class FakeOrbSlot : MonoBehaviour
 {
     private NomaiInterfaceOrb _orb;
     private bool _wasBeingDragged;
     private Vector3 _localLockPos;
+    
+    private Vector3 _debugVelocity;
 
-    private void Awake()
+    private void Start()
     {
         _orb = GetComponent<NomaiInterfaceOrb>();
         _localLockPos = _orb._orbBody.GetOrigParent().InverseTransformPoint(_orb.transform.position);
     }
 
-    private void FixedUpdate()
+    [HarmonyPrefix, HarmonyPatch(typeof(NomaiInterfaceOrb), nameof(NomaiInterfaceOrb.FixedUpdate))]
+    private static void NomaiInterfaceOrb_FixedUpdate(NomaiInterfaceOrb __instance)
+    {
+        var @this = __instance.GetComponent<FakeOrbSlot>();
+        if (@this) @this.PreOrbFixedUpdate();
+    }
+
+    private void PreOrbFixedUpdate()
     {
         var isBeingDragged = _orb._isBeingDragged;
 
         if (_wasBeingDragged && !isBeingDragged)
         {
-            _localLockPos = _orb._localTargetPos;
+            // _localLockPos = _orb._localTargetPos;
+            _localLockPos = _orb._orbBody.GetOrigParent().InverseTransformPoint(_orb.transform.position);
         }
 
         if (!isBeingDragged)
         {
             _orb.MoveTowardPosition(_orb._orbBody.GetOrigParent().TransformPoint(_localLockPos));
         }
-        
+
         _wasBeingDragged = isBeingDragged;
+
+        _debugVelocity = _orb._orbBody.GetVelocity();
+    }
+
+    private void OnGUI()
+    {
+        GUILayout.Label($"orb {_orb} vel = {_debugVelocity}");
     }
 }
