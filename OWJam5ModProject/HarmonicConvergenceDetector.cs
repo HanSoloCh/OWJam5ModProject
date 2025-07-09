cfab5e0df45f720de518b9f5d431b304c0fea4e8
2walker2 <dwalkermcdonald@gmail.com>
2025-07-09 19:19:53 -0400

    Harmonic convergence puzzle almost done

--- OWJam5ModProject/HarmonicConvergenceDetector.cs
+++ OWJam5ModProject/HarmonicConvergenceDetector.cs
@@ -1,4 +1,5 @@
 ﻿using HarmonyLib;
+using NewHorizons;
 using System;
 using System.Collections.Generic;
 using UnityEngine;
@@ -10,17 +11,68 @@ namespace OWJam5ModProject
         string HARMONIC_FREQUENCY = "Walker_Jam5_Harmonic";
         float HARMONIC_THRESHOLD = 0.7f;
 
-        List<AudioSignal> harmonicSignals;
+        [SerializeField] string[] signalNames;
+        [SerializeField] Light[] detectorLights;
+        [SerializeField] MeshRenderer[] detectorLightRenderers;
+        [SerializeField] GameObject computerParent;
+        [SerializeField] OWTriggerVolume playerDetectorTrigger;
+
+        AudioSignal[] harmonicSignals;
+        bool playerInside;
 
         void Start()
         {
+            // Get signals
             List<AudioSignal> audioSignals = Locator.GetAudioSignals();
-            harmonicSignals = new List<AudioSignal>();
+            harmonicSignals = new AudioSignal[signalNames.Length];
             foreach (AudioSignal signal in audioSignals)
             {
                 if (signal.GetFrequency().ToString() == HARMONIC_FREQUENCY)
-                    harmonicSignals.Add(signal);
+                {
+                    for (int i=0; i<signalNames.Length; i++)
+                    {
+                        if (signal.GetName().ToString() == signalNames[i])
+                        {
+                            harmonicSignals[i] = signal;
+                            signal.GetComponent<AudioSignalDetectionTrigger>()._allowUnderwater = true;
+                            break;
+                        }
+                    }
+                }
             }
+
+            // Initialize detector
+            playerDetectorTrigger.OnEntry += PlayerDetector_OnEntry;
+            playerDetectorTrigger.OnExit += PlayerDetector_OnExit;
+        }
+
+        void Update()
+        {
+            if (playerInside)
+            {
+                if (CheckConvergence())
+                    OWJam5ModProject.Instance.ModHelper.Console.WriteLine("Converged!!!");
+            }
+        }
+
+        void OnDestroy()
+        {
+            playerDetectorTrigger.OnEntry -= PlayerDetector_OnEntry;
+            playerDetectorTrigger.OnExit -= PlayerDetector_OnExit;
+        }
+
+        private void PlayerDetector_OnEntry(GameObject hitObj)
+        {
+            //OWJam5ModProject.Instance.ModHelper.Console.WriteLine(hitObj.ToString() + " entered");
+            //OWJam5ModProject.Instance.ModHelper.Console.WriteLine(Locator.GetPlayerDetector().ToString() + " compared");
+            if (hitObj == Locator.GetPlayerDetector())
+                playerInside = true;
+        }
+
+        private void PlayerDetector_OnExit(GameObject hitObj)
+        {
+            if (hitObj == Locator.GetPlayerDetector())
+                playerInside = false;
         }
 
         bool CheckConvergence()
