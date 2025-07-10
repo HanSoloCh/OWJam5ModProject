dc47ef75b64b18f0f290432f5f5d68272d004f8d
2walker2 <dwalkermcdonald@gmail.com>
2025-07-10 16:23:26 -0400

    Ghost matter cave

---
+++ OWJam5ModProject/FluidDetectorAssigner.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UnityEngine;
+
+namespace OWJam5ModProject
+{
+    internal class FluidDetectorAssigner : MonoBehaviour
+    {
+        void Start()
+        {
+            ConstantFluidDetector fluidDetector = GetComponent<ConstantFluidDetector>();
+            FluidVolume fluidVolume = transform.root.Find("Sector/Water").GetComponentInChildren<FluidVolume>();
+            fluidDetector._onlyDetectableFluid = fluidVolume;
+        }
+    }
+}
