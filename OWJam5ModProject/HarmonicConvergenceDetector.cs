0de0c24d4c65c09afb299344f47cc0d61406e8a3
2walker2 <dwalkermcdonald@gmail.com>
2025-07-09 20:53:27 -0400

    Harmonic convergence polish

--- OWJam5ModProject/HarmonicConvergenceDetector.cs
+++ OWJam5ModProject/HarmonicConvergenceDetector.cs
@@ -10,10 +10,14 @@ namespace OWJam5ModProject
     {
         string HARMONIC_FREQUENCY = "Walker_Jam5_Harmonic";
         float HARMONIC_THRESHOLD = 0.7f;
+        string EMISSION_COLOR_PROPERTY = "_EmissionColor";
+        float TORCH_FADE_DURATION = 0.25f;
 
         [SerializeField] string[] signalNames;
         [SerializeField] Light[] detectorLights;
         [SerializeField] MeshRenderer[] detectorLightRenderers;
+        [SerializeField] OWAudioSource[] detectorLightPowerOnAudioSources;
+        [SerializeField] OWAudioSource[] detectorLightPowerOffAudioSources;
         [SerializeField] GameObject computerParent;
         [SerializeField] OWTriggerVolume playerDetectorTrigger;
 
@@ -21,6 +25,10 @@ namespace OWJam5ModProject
         NomaiComputer computer;
         bool playerInside;
         bool convergenceSolved;
+        bool[] torchesActive;
+        float[] torchFadeValues;
+        float torchBrightness;
+        Color torchColor;
 
         void Start()
         {
@@ -49,6 +57,16 @@ namespace OWJam5ModProject
 
             // Initialize computer
             NewHorizons.Utility.OWML.Delay.FireOnNextUpdate(InitializeComputer);
+
+            // Initialize torches
+            torchesActive = new bool[detectorLights.Length];
+            
+            torchFadeValues = new float[detectorLights.Length];
+            for (int i = 0; i < torchFadeValues.Length; i++)
+                torchFadeValues[i] = 1;
+
+            torchBrightness = detectorLights[0].intensity;
+            torchColor = detectorLightRenderers[0].material.GetColor(EMISSION_COLOR_PROPERTY);
         }
 
         void InitializeComputer()
@@ -67,6 +85,8 @@ namespace OWJam5ModProject
                     computer.DisplayAllEntries();
                 }
             }
+
+            UpdateTorches();
         }
 
         void OnDestroy()
@@ -91,13 +111,43 @@ namespace OWJam5ModProject
 
         bool CheckConvergence()
         {
-            foreach (AudioSignal signal in harmonicSignals)
+            bool result = true;
+
+            for (int i=0; i<harmonicSignals.Length; i++)
             {
-                if (signal.GetSignalStrength() < HARMONIC_THRESHOLD)
-                    return false;
+                bool signalPlaying = harmonicSignals[i].GetSignalStrength() >= HARMONIC_THRESHOLD;
+
+                if (torchesActive[i] != signalPlaying)
+                {
+                    if (signalPlaying)
+                        detectorLightPowerOnAudioSources[i].Play();
+                    else
+                        detectorLightPowerOffAudioSources[i].Play();
+                }
+
+                torchesActive[i] = signalPlaying;
+
+                if (signalPlaying == false)
+                    result = false;
             }
 
-            return true;
+            return result;
+        }
+
+        void UpdateTorches()
+        {
+            for (int i=0; i<torchesActive.Length; i++)
+            {
+                float targetFadeValue = torchesActive[i] ? 1 : 0;
+                float newFadeValue = Mathf.MoveTowards(torchFadeValues[i], targetFadeValue, Time.deltaTime / TORCH_FADE_DURATION);
+
+                if (newFadeValue != torchFadeValues[i])
+                {
+                    torchFadeValues[i] = newFadeValue;
+                    detectorLightRenderers[i].material.SetColor(EMISSION_COLOR_PROPERTY, Color.Lerp(Color.black, torchColor, torchFadeValues[i]));
+                    detectorLights[i].intensity = Mathf.Lerp(0, torchBrightness, torchFadeValues[i]);
+                }
+            }
         }
     }
 }
