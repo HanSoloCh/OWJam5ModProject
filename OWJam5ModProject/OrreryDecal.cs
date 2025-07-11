e1fbff4d74ddc183dd5bf59c6f1c62da1497728f
2walker2 <dwalkermcdonald@gmail.com>
2025-07-11 13:35:52 -0400

    Orrery icons rotate to face center

---
+++ OWJam5ModProject/OrreryDecal.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UnityEngine;
+
+namespace OWJam5ModProject
+{
+    internal class OrreryDecal : MonoBehaviour
+    {
+        [SerializeField] Transform orreryCenter;
+
+        void Update()
+        {
+            transform.LookAt(orreryCenter, orreryCenter.transform.up);
+        }
+    }
+}
