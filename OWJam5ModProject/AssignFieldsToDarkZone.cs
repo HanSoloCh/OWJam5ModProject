b1c958f75eedd412f5a9373cc2537ebe6b117058
gpixl <greenpixel16@gmail.com>
2025-07-13 23:46:32 -0400

    darkzone script

---
+++ OWJam5ModProject/AssignFieldsToDarkZone.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace OWJam5ModProject
+{
+    public class AssignFieldsToDarkZone : MonoBehaviour
+    {
+        public DarkZone dz;
+
+        public void Start()
+        {
+            dz._ambientLight = transform.root.Find("Sector/AmbientLight").GetComponent<Light>();
+            dz._planetaryFog = transform.root.Find("Sector/FogSphere").GetComponent<PlanetaryFogController>();
+            dz.Awake(); //gotta call this again so it gets the right values instead of getting them before NH makes them
+        }
+    }
+}
