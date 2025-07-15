de5a9c83e772346f29cb7f9d07fa321c603b5b72
2walker2 <dwalkermcdonald@gmail.com>
2025-07-14 22:25:44 -0400

    Quantum rock text and ship logs

---
+++ OWJam5ModProject/RemoveChildren.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace OWJam5ModProject
+{
+    internal class RemoveChildren : MonoBehaviour
+    {
+        [SerializeField] string[] children;
+
+        void Start()
+        {
+            foreach (string childPath in children)
+            {
+                Transform child = transform.Find(childPath);
+                Destroy(child.gameObject);
+            }
+        }
+    }
+}
