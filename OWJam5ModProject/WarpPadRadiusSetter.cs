f2d112b3be7380ae0cf5b0c92d6e630388950160
2walker2 <dwalkermcdonald@gmail.com>
2025-07-16 00:42:14 -0400

    Ernesto hint satellite

---
+++ OWJam5ModProject/WarpPadRadiusSetter.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace OWJam5ModProject
+{
+    internal class WarpPadRadiusSetter : MonoBehaviour
+    {
+        [SerializeField] float warpRadius;
+
+        void Start()
+        {
+            NomaiWarpPlatform warpPlatform = GetComponentInChildren<NomaiWarpPlatform>();
+            warpPlatform._warpRadius = warpRadius;
+        }
+    }
+}
