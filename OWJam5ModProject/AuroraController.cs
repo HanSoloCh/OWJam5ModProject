08ec93deee08cef765a44e518c381a252947ffaa
lezzlebit <greenpixel16@gmail.com>
2025-07-16 23:47:03 -0400

    Create AuroraController.cs

---
+++ OWJam5ModProject/AuroraController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace OWJam5ModProject
+{
+    public class AuroraController : MonoBehaviour
+    {
+        public MeshRenderer render;
+        public Transform scaleRead;
+        public float defaultLocalScale;
+
+        public void Update()
+        {
+            render.material.SetFloat("_Intensity", (scaleRead.localScale.x - 160f) / 36f);
+        }
+    }
+}
