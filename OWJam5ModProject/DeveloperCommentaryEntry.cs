72a0e0fc4cfbf1c29163ef0c06e625ee2cefe6ee
2walker2 <dwalkermcdonald@gmail.com>
2025-07-29 19:30:10 -0400

    Started work on developer commentary

---
+++ OWJam5ModProject/DeveloperCommentaryEntry.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using UnityEngine;
+using OWML.Utils;
+using NewHorizons.Components.Props;
+
+namespace OWJam5ModProject
+{
+    internal class DeveloperCommentaryEntry : MonoBehaviour
+    {
+        const string SIGNAL_FREQUENCY_NAME = "Developer Commentary";
+        const string SIGNAL_AUDIO = "TH_RadioSignal_LP";
+
+        [Serializable]
+        public struct CommentaryDemonstration
+        {
+            public string nodeName;
+            public int dialogPage;
+            public Transform attentionPoint;
+            public Transform enableRoot;
+        }
+
+        [Header("Required References")]
+        [SerializeField] NHCharacterDialogueTree dialogTree;
+
+        [Header("Settings")]
+        [SerializeField] string signalName = "COMMENTARY TOPIC";
+        [SerializeField] CommentaryDemonstration[] commentaryDemonstrations;
+
+        AudioSignal signal;
+
+        void Start()
+        {
+            signal = OWJam5ModProject.Instance.NewHorizons.SpawnSignal(OWJam5ModProject.Instance, gameObject, SIGNAL_AUDIO, signalName, SIGNAL_FREQUENCY_NAME, identificationRadius: 3);
+        }
+    }
+}
