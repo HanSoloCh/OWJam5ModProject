c966efba1d0435196dc0a555613722838c801177
2walker2 <dwalkermcdonald@gmail.com>
2025-07-29 15:07:20 -0400

    Orrery station lights now stay on after first visit each loop

---
+++ OWJam5ModProject/StationLightsController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+namespace OWJam5ModProject
+{
+    internal class StationLightsController : MonoBehaviour
+    {
+        [SerializeField] OWTriggerVolume playerDetectorTrigger = null;
+        [SerializeField] float fadeDuration;
+
+        Light[] lights;
+        float[] lightsInitialIntensities;
+
+        void Start()
+        {
+            playerDetectorTrigger.OnEntry += PlayerDetectorTrigger_OnEntry;
+
+            lights = GetComponentsInChildren<Light>();
+
+            lightsInitialIntensities = new float[lights.Length];
+            for (int i=0; i<lights.Length; i++)
+            {
+                lightsInitialIntensities[i] = lights[i].intensity;
+                lights[i].intensity = 0;
+            }
+        }
+
+        private void PlayerDetectorTrigger_OnEntry(GameObject hitObj)
+        {
+            if (hitObj.CompareTag("PlayerDetector"))
+                StartCoroutine(FadeLights());
+        }
+
+        IEnumerator FadeLights()
+        {
+            float t = 0;
+            while (t < 1)
+            {
+                for (int i=0; i<lights.Length; i++)
+                {
+                    lights[i].intensity = Mathf.Lerp(0, lightsInitialIntensities[i], t);
+                }
+
+                t += Time.deltaTime / fadeDuration;
+                yield return new WaitForEndOfFrame();
+            }
+
+            for (int i = 0; i < lights.Length; i++)
+            {
+                lights[i].intensity = lightsInitialIntensities[i];
+            }
+        }
+    }
+}
